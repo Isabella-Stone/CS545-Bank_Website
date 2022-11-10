f7e66b5e336913e4c2aea51887613ece179eb3ff
megxsan <msanford@stevens.edu>
2022-11-09 20:58:22 -0500

    account js attempt

---
+++ js/account.js
@@ -0,0 +1,13 @@
+function returnName(){
+    var inputVal = document.getElementById("name").innerHTML;
+    alert("HI");
+}
+
+function returnTel(){
+    let newTel = document.getElementById("tel").value;
+    return newTel;
+}
+
+function returnEmail(){
+    
+}
