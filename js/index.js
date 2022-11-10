3fb2b61c2a69a7c329fd631eb64c97b16b4ad77b
mdardir <mariamdardir@gmail.com>
2022-11-09 20:40:05 -0500

    added updates to the cashback rweard system

--- js/index.js
+++ js/index.js
@@ -5,11 +5,27 @@ function depositClick() {
     }
     document.getElementById("balance").innerHTML = parseInt(document.getElementById("balance").innerHTML) + parseInt(amt);
 }
-
+var amount = 20;
 function withdrawClick() {
     var amt = prompt("How much would you like to withdraw?");
     if (amt === null) {
         return;
     }
     document.getElementById("balance").innerHTML = parseInt(document.getElementById("balance").innerHTML) - parseInt(amt);
+    amount-=parseInt(amt);
+    document.getElementById("amount_left").innerHTML = amount;
 }
+
+// var width = 80;
+// var base = setInterval(updateBar, 10)
+// function pauseProgress() {
+//     if (width >= 100){
+//         clearInterval(base);
+//     }
+//     else{
+//         width++;
+//         element.style.width = width + '%';
+//         element.innerHTML = width * 1+ '%';
+//     }
+// }
+// pauseProgress();
