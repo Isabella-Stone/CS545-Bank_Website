d02f5b6e07c5553a1fb11b21f94ecaa65ad4afaf
Isabella Stone <istone@stevens.edu>
2022-11-09 20:48:00 -0500

    update withdraw error

--- js/index.js
+++ js/index.js
@@ -8,8 +8,13 @@ function depositClick() {
 
 function withdrawClick() {
     var amt = prompt("How much would you like to withdraw?");
-    if (amt === null) {
+    if (amt === null ) {
+        return;
+    }
+    if (parseInt(amt) > parseInt(document.getElementById("balance").innerHTML)) {
+        alert("You cannot withdraw more money than what is currently in your account.");
         return;
     }
     document.getElementById("balance").innerHTML = parseInt(document.getElementById("balance").innerHTML) - parseInt(amt);
 }
+
