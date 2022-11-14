b500e37c7aad57b96d5649b2b27cbb3847c3f7d9
megxsan <msanford@stevens.edu>
2022-11-14 12:56:19 -0500

    added error checkng for account

--- js/account.js
+++ js/account.js
@@ -1,11 +1,28 @@
 function returnName(){
-    document.getElementById("name").innerHTML = document.getElementById("newname").value;
+    var format = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    if(document.getElementById("newname").value.match(format)){
+        document.getElementById("name").innerHTML = document.getElementById("newname").value;    }
+    else{
+        alert("You have entered an invalid first and last name");
+    }
 }
 
 function returnTel(){
-    document.getElementById("tel").innerHTML = document.getElementById("newtel").value;
+    var format = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    if(document.getElementById("newtel").value.match(format)){
+        document.getElementById("tel").innerHTML = document.getElementById("newtel").value;
+    }
+    else{
+        alert("You have entered an invalid phone number");
+    }
 }
 
 function returnEmail(){
-    document.getElementById("email").innerHTML = document.getElementById("newemail").value;
+    var format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (document.getElementById("newemail").value.match(format)){
+        document.getElementById("email").innerHTML = document.getElementById("newemail").value;
+    }
+    else{
+        alert("You have entered an invalid email address");
+    }
 }
