64b57e97d5ef08013b81aac6bef5539b420a2d50
rgiova27 <rcgiovanniello@gmail.com>
2022-12-03 16:40:14 -0500

    storage for cashback variables

--- js/index.js
+++ js/index.js
@@ -10,8 +10,21 @@ window.onload = function getValues() {
     if (sessionStorage.getItem("valUpdated") != 'true')
     {
         localStorage.setItem("storeBalance", 104600.52);
+        
+        localStorage.setItem("storeCounter", document.getElementById("counter").innerHTML);
+        localStorage.setItem("storeUpdate", document.getElementById("update").innerHTML);
+        localStorage.setItem("storeCBR", document.getElementById("cbr").value);
+        localStorage.setItem("storeAmtLeft", document.getElementById("amount_left").innerHTML);
+        localStorage.setItem("storeAmtEarned", document.getElementById("amount_earned").innerHTML);
+        //localStorage.setItem("storePercent", document.getElementById("percent").innerHTML);
     }
     document.getElementById("balance").innerHTML = localStorage.getItem("storeBalance");
+    document.getElementById("counter").innerHTML = localStorage.getItem("storeCounter");
+    document.getElementById("update").innerHTML = localStorage.getItem("storeUpdate");
+    document.getElementById("cbr").value = localStorage.getItem("storeCBR");
+    document.getElementById("amount_left").innerHTML = localStorage.getItem("storeAmtLeft");
+    document.getElementById("amount_earned").innerHTML = localStorage.getItem("storeAmtEarned");
+    //document.getElementById("percent").innerHTML = localStorage.getItem("storePercent");
 }
 function depositClick() {
     var amt = prompt("How much would you like to deposit?");
@@ -23,6 +36,7 @@ function depositClick() {
 
     balance = parseFloat(document.getElementById("balance").innerHTML) + parseFloat(amt);
     document.getElementById("balance").innerHTML = balance;
+    
     localStorage.setItem("storeBalance", balance);
     sessionStorage.setItem("valUpdated", 'true');
 }
@@ -46,8 +60,7 @@ function withdrawClick() {
     }
     balance = parseFloat(document.getElementById("balance").innerHTML) - parseFloat(amt);
     document.getElementById("balance").innerHTML = balance;
-    sessionStorage.setItem("valUpdated", 'true');
-    localStorage.setItem("storeBalance", balance);
+
 
     if(parseFloat(amt) < parseFloat(amount)){
         amount -= parseInt(amt);
@@ -64,12 +77,24 @@ function withdrawClick() {
     document.getElementById("cbr").value = 100-amount;
     document.getElementById("amount_left").innerHTML = amount;
     document.getElementById("amount_earned").innerHTML = cb.toFixed(2);
-    document.getElementById("percent").innerHTML = amt;    
+    //document.getElementById("percent").innerHTML = amt;    
+    
+    localStorage.setItem("storeBalance", balance);
+    localStorage.setItem("storeCounter", document.getElementById("counter").innerHTML);
+    localStorage.setItem("storeUpdate", document.getElementById("update").innerHTML);
+    localStorage.setItem("storeCBR", document.getElementById("cbr").value);
+    localStorage.setItem("storeAmtLeft", document.getElementById("amount_left").innerHTML);
+    localStorage.setItem("storeAmtEarned", document.getElementById("amount_earned").innerHTML);
+    sessionStorage.setItem("valUpdated", 'true');
 }
 
 function RedeemClick() {
     document.getElementById("amount_earned").innerHTML = 0;
     cb = 0;
     document.getElementById("cbr").value = 0;
+
+    localStorage.setItem("storeAmtEarned", document.getElementById("amount_earned").innerHTML);
+    localStorage.setItem("storeCBR", document.getElementById("cbr").value);
+    sessionStorage.setItem("valUpdated", 'true');
 }
 
